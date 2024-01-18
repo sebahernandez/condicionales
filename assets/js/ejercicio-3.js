d0ee7f9db2d3d96f697ec133c5c584eb7c282371
sebahernandez <sebaprogramer@gmail.com>
2024-01-18 15:52:14 -0300

    fix a password

--- assets/js/ejercicio-3.js
+++ assets/js/ejercicio-3.js
@@ -6,11 +6,15 @@ const select2 = document.querySelector("#select2");
 const select3 = document.querySelector("#select3");
 
 const password = 911;
+const password2 = 714;
 
 const action = () => {
   const suma = Number(select1.value + select2.value + select3.value);
   if (suma === password) {
-    mensaje.innerHTML = "La contraseña es correcta";
+    mensaje.innerHTML = "password 1 correcto";
+    return;
+  } else if (suma === password2) {
+    mensaje.innerHTML = "password 2 correcto";
     return;
   }
   mensaje.innerHTML = "La contraseña es incorrecta";
